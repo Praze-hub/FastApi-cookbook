e85e77272423add998b34ad2fcc4aa91904d8aaa
Praze-hub <godspraiseokechukwu07@gmail.com>
2025-09-17 08:08:27 +0100

    Learnt about user creation and how to create and migrate users

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from src.books.routes import book_router
 from contextlib import asynccontextmanager
 from src.db.main import init_db
+from src.auth.routes import auth_router
 
 @asynccontextmanager
 async def life_span(app:FastAPI):
@@ -19,4 +20,5 @@ app = FastAPI(
     lifespan=life_span
 )
 
-app.include_router(book_router, prefix=f"/api/{version}/books")
+app.include_router(book_router, prefix=f"/api/{version}/books", tags=['books'])
+app.include_router(auth_router, prefix=f"/api/{version}/auth", tags=['auth'])
--- src/auth/models.py
+++ src/auth/models.py
@@ -17,6 +17,7 @@ class User(SQLModel, table=True):
     email: str
     last_name: str
     is_verified: bool = Field(default=False)
+    password_hash: str = Field(exclude=True)
     created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP(timezone=True), default=datetime.utcnow))
     updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP(timezone=True), default=datetime.utcnow, onupdate=datetime.utcnow))
     
