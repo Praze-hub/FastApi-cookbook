21fda4a3cb69db95fab19178ec87c81c45df39a7
Praze-hub <godspraiseokechukwu07@gmail.com>
2025-09-20 11:37:48 +0100

    Learnt how to generate new token to renew a users session

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,5 +1,6 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 from fastapi import APIRouter, Depends, HTTPException, status
+from src.auth.dependencies import RefreshTokenBearer
 from src.db.main import get_session
 from .schemas import UserCreateModel, UserModel, UserLoginModel
 from .service import UserService
@@ -35,50 +36,57 @@ async def create_user_account(
 
     return new_user
 
-@auth_router.post('/login')
-async def login_users(login_data: UserLoginModel, session:AsyncSession = Depends(get_session)):
+
+@auth_router.post("/login")
+async def login_users(
+    login_data: UserLoginModel, session: AsyncSession = Depends(get_session)
+):
     email = login_data.email
-    password=login_data.password
-    
+    password = login_data.password
+
     user = await user_service.get_user_by_email(email, session)
-    
+
     if user is not None:
         password_valid = verify_password(password, user.password_hash)
-        
+
         if password_valid:
             access_token = create_access_token(
-                user_data={
-                    'email': user.email,
-                    'user_uid': str(user.uid)
-                },
-                expiry=timedelta(seconds=ACCESS_TOKEN_EXPIRY)
+                user_data={"email": user.email, "user_uid": str(user.uid)},
+                expiry=timedelta(seconds=ACCESS_TOKEN_EXPIRY),
             )
-            
+
             refresh_token = create_access_token(
-                user_data={
-                    'email': user.email,
-                    'user_uid': str(user.uid)
-                },
-                
-                refresh = True,
-                expiry=timedelta(days=REFRESH_TOKEN_EXPIRY)
+                user_data={"email": user.email, "user_uid": str(user.uid)},
+                refresh=True,
+                expiry=timedelta(days=REFRESH_TOKEN_EXPIRY),
             )
-            
+
             return JSONResponse(
                 content={
-                    'message': 'Login successful',
-                    'access_token':access_token,
-                    'refresh_token':refresh_token,
-                    'user':{
-                        'email': user.email,
-                        'uid': str(user.uid)
-                    }
+                    "message": "Login successful",
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
+                    "user": {"email": user.email, "uid": str(user.uid)},
                 }
             )
-            
+
     raise HTTPException(
-        status_code=status.HTTP_403_FORBIDDEN,
-        detail='Invalid email or password'
+        status_code=status.HTTP_403_FORBIDDEN, detail="Invalid email or password"
     )
-            
-            
+    
+@auth_router.get('/refresh_token')
+async def get_new_access_token(token_details:dict = Depends(RefreshTokenBearer())):
+    expiry_timestamp = token_details['exp']
+    
+    #checks for the expiry of the access token
+    if datetime.fromtimestamp(expiry_timestamp) > datetime.now():
+        new_access_token = create_access_token(
+            user_data=token_details['user']
+        )
+        
+        return JSONResponse(content={
+            "access_token": new_access_token
+        })
+        
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, default='Invalid or expired token')
+
